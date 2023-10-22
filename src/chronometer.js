18b3312ab5805d61d4a5fa6e3caa623c9a563c00
Ana Filipa <anafilipa737@gmail.com>
2023-10-22 20:44:29 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,64 @@
 class Chronometer {
+
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime = 0;
+    this.intervalId = null;
+    }
+  
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval( () => {
+      this.currentTime += 1
+
+      if (printTimeCallback){  // if prinTimeCallBack exists...
+        printTimeCallback();  // chamar a própria função. Se não existe - null.
+      }
+         }, 1000);
+
+      // this.intervalId = setInterval( () => {
+      // this.currentTime += 1 // 1 centisegundo = 10 milisegundos
+
+      // if (printTimeCallback){  // if prinTimeCallBack exists...
+      //   printTimeCallback();  // chamar a própria função. Se não existe - null.
+      // }
+      //    }, 10); //aumentar um centisegundo a cada 10 milisegundos
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
+    // return Math.floor(this.currentTime / 100 / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
+    // return Math.floor(this.currentTime / 100 % 60)
   }
 
+  // getCentisegundos(){
+  //   return Math.floor(this.currentTime % 100)
+  // }
+
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // if (value<10){
+    //   return "0" + value; 
+    // }
+    // else {
+    //   return '' + value;  // se um dos argumentos da soma for string, retorna string.
+    // }
+
+    return ("0"+value).slice(-2)
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds())
+    // return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds()) + ":" + this.computeTwoDigitNumber(this.getCentisegundos())
   }
 }
